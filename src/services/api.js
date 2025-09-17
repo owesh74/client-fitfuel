f7ce961862599e9189c24076e3e402d20757400d
owesh74 <oweshkhan1856@gmail.com>
2025-09-17 17:12:05 +0530

    added api

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Your backend URL
+  // This uses the variable you set in the Vercel dashboard
+  baseURL: `${process.env.REACT_APP_API_URL}/api`, 
   headers: {
     'Content-Type': 'application/json',
   },
 });
-
 // Interceptor to add the token to every request if it exists
 api.interceptors.request.use(
   (config) => {
